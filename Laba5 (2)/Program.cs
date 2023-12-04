4535f4abe4bf5ec8ca75b82d1c0c362aa259af3a
Masha <huivaniuk.mariia@chnu.edu.ua>
2023-12-04 11:13:35 +0100

    Add project files.

---
+++ Laba5 (2)/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("2");
+
+        Console.WriteLine("number= ");
+        int number=Convert.ToInt32(Console.ReadLine());
+
+        bool isPrime = IsPrime(number);
+        if (isPrime)
+        {
+            Console.WriteLine("Число є простим " + number);
+        }
+        else
+        {
+           Console.WriteLine("Число не є простим " +number);
+        }
+
+    }
+    static bool IsPrime(int n)
+    {
+        if (n<=1)
+        {
+            return false;
+        }
+        for (int i = 2; i<=Math.Sqrt(n); i++)
+        {
+            if (n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
